f522acc69fe90a79011d9ac13ebdc5d23b247c67
reks-G <kabanqwefordora@gmail.com>
2026-01-10 19:43:06 +0700

    Fix: serve files from src folder, update CSP for Render

--- server.js
+++ server.js
@@ -92,10 +92,36 @@ setInterval(saveAll, 30000);
 process.on('SIGINT', () => { saveAll(); process.exit(); });
 process.on('SIGTERM', () => { saveAll(); process.exit(); });
 
+const MIME_TYPES = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon'
+};
+
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('MrDomestos* Server');
+  
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(__dirname, 'src', filePath);
+  
+  const ext = path.extname(filePath);
+  const contentType = MIME_TYPES[ext] || 'application/octet-stream';
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
 });
 
 const wss = new WebSocket.Server({ server });
